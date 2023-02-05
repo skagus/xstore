d7c64331db482709120f66c99f562bcbe4cd58eb
NH <skagus@NH>
2023-02-05 13:44:10 +0900

    Works well all.

--- main/main.c
+++ main/main.c
@@ -64,19 +64,42 @@ void buf_dump(uint8_t* pBuf, uint32_t nSize)
 uint8_t aRBuf[BUF_SIZE];
 uint8_t aTBuf[BUF_SIZE];
 
+typedef struct _XCtx
+{
+	FILE* pFile;
+	uint32_t nBytes;
+} XCtx;
+
 void txData(void* pCtx, uint32_t nOffset, uint8_t* pDstBuf, uint32_t nBytes)
 {
-	memcpy(pDstBuf, &(aTBuf[nOffset]), nBytes);
+	XCtx* pXCtx = (XCtx*)pCtx;
+	if(NULL != pXCtx->pFile)
+	{
+		fread(pDstBuf, 1, nBytes, pXCtx->pFile);
+	}
+	else
+	{
+		memcpy(pDstBuf, &(aTBuf[nOffset]), nBytes);
+	}
 }
 
 void rxData(void* pCtx, uint32_t nOffset, uint8_t* pSrcBuf, uint32_t nBytes)
 {
-	memcpy(&(aRBuf[nOffset]), pSrcBuf, nBytes);
+	XCtx* pXCtx = (XCtx*)pCtx;
+	pXCtx->nBytes += nBytes;
+	if(NULL != pXCtx->pFile)
+	{
+		fwrite(pSrcBuf, 1, nBytes, pXCtx->pFile);
+	}
+	else
+	{
+		memcpy(&(aRBuf[nOffset]), pSrcBuf, nBytes);
+	}
 }
 
 void app_main(void)
 {
-	ESP_LOGI(TAG, "Start APP");
+
 	//	uart_set_baudrate(CONFIG_ESP_CONSOLE_UART_NUM, 115200);
 	setvbuf(stdin, NULL, _IONBF, 0);
 	setvbuf(stdout, NULL, _IONBF, 0);
@@ -85,8 +108,28 @@ void app_main(void)
 	esp_vfs_dev_uart_set_rx_line_endings(ESP_LINE_ENDINGS_CR);
 	esp_vfs_dev_uart_set_rx_line_endings(ESP_LINE_ENDINGS_CRLF);
 
-	const char *szLine = "Hello World..\n";
-	uart_tx_chars(CONFIG_ESP_CONSOLE_UART_NUM, szLine, strlen(szLine));
+
+	esp_vfs_spiffs_conf_t conf = {
+      .base_path = "/spiffs",
+      .partition_label = NULL,
+      .max_files = 5,
+      .format_if_mount_failed = true
+    };
+    esp_err_t ret = esp_vfs_spiffs_register(&conf);
+
+
+
+	// Use POSIX and C standard library functions to work with files.
+	// First create a file.
+	ESP_LOGI(TAG, "Opening file");
+	FILE* f = fopen("/spiffs/test.txt", "w");
+	if (f == NULL)
+	{
+		ESP_LOGE(TAG, "Failed to open file for writing");
+		return;
+	}
+	fprintf(f, "Hello World!\n");
+	fclose(f);
 
 	for(int i=0; i< BUF_SIZE; i++)
 	{
@@ -110,11 +153,14 @@ void app_main(void)
 			buf_dump(aRBuf, BUF_SIZE);
 			printf("\n");
 		}
-		else if(CMD_COMP(aArgs[0], "tbuf"))
+		else if(CMD_COMP(aArgs[0], "info"))
 		{
-			printf("Dump S Buf\n");
-			buf_dump(aTBuf, BUF_SIZE);
-			printf("\n");
+			size_t total = 0, used = 0;
+			ret = esp_spiffs_info(NULL, &total, &used);
+			if (ret == ESP_OK)
+			{
+				printf("Partition size: total: %d, used: %d", total, used);
+			}
 		}
 		else if(CMD_COMP(aArgs[0], "tx"))
 		{
@@ -122,11 +168,40 @@ void app_main(void)
 			nRet = XM_Transmit(txData, NULL, BUF_SIZE);
 			printf("Ret: %d\n", nRet);
 		}
+		else if(CMD_COMP(aArgs[0], "fdump"))
+		{
+			FILE* pFile = fopen("/spiffs/test.bin", "r");
+			if(NULL != pFile)
+			{
+				uint8_t aBuf[32];
+				int nBytes;
+				printf("\n");
+
+				while(1)
+				{
+					nBytes = fread(aBuf, 1, 32, pFile);
+					if(nBytes <= 0)
+					{
+						break;
+					}
+					for(uint8_t nIdx = 0; nIdx < nBytes; nIdx++)
+					{
+						printf("%2X ", aBuf[nIdx]);
+					}
+					printf("\n");
+				}
+				fclose(pFile);
+			}
+		}
 		else if(CMD_COMP(aArgs[0], "rx"))
 		{
 			printf("Start to PC -> T\n");
-			nRet = XM_Receive(rxData, NULL, BUF_SIZE);
-			printf("Ret: %d\n", nRet);
+			XCtx stRxCtx;
+			stRxCtx.pFile = fopen("/spiffs/test.bin", "w");
+			stRxCtx.nBytes = 0;
+			nRet = XM_Receive(rxData, &stRxCtx, BUF_SIZE);
+			fclose(stRxCtx.pFile);
+			printf("Ret: %d, %d\n", nRet, stRxCtx.nBytes);
 		}
 		else if(CMD_COMP(aArgs[0], "dbg"))
 		{

ec8e5e8a5bb70aac820767ecc20a769525faabc3
skagus <skagus@gmail.com>
2024-06-02 07:48:35 +0900

    LittleFS 적용.(Partial working)
    file list, delete 동작 안함.

--- main/dos.c
+++ main/dos.c
@@ -3,6 +3,8 @@
 
 #include "esp_err.h"
 #include "esp_spiffs.h"
+#include "esp_littlefs.h"
+
 #include "esp_vfs_dev.h"
 
 #include "uart.h"
@@ -10,19 +12,32 @@
 #include "dos.h"
 
 #define MAX_PATH_LEN		(260)
+#define FS_LFS				(0)
 
+#if (FS_LFS)
+#define FS_LABEL			"littlefs"
+#else
 #define FS_LABEL			"storage"
+#endif
 
 void _fs_format(uint8_t argc,char* argv[])
 {
 	printf("It takes a few seconds / minutes\n");
+#if (FS_LFS)
+	esp_littlefs_format(FS_LABEL);
+#else
 	esp_spiffs_format(FS_LABEL);
+#endif
 }
 
 void _fs_info(uint8_t argc,char* argv[])
 {
 	size_t total = 0,used = 0;
+#if (FS_LFS)
+	int ret = esp_littlefs_info(FS_LABEL,&total,&used);
+#else
 	int ret = esp_spiffs_info(FS_LABEL,&total,&used);
+#endif
 	if(ret == ESP_OK)
 	{
 		printf("Partition size: total: %d, used: %d",total,used);
@@ -41,13 +56,11 @@ void _fs_write(uint8_t argc,char* argv[])
 		return;
 	}
 	printf("This is 'copy con'\n");
-	char szPath[MAX_PATH_LEN];
-	sprintf(szPath,"/sf/%s",argv[1]);
 
 	char nRcv;
 	// Use POSIX and C standard library functions to work with files.
 	// First create a file.
-	FILE* f = fopen(szPath,"w");
+	FILE* f = fopen(argv[1],"w");
 
 	if(NULL != f)
 	{
@@ -68,25 +81,38 @@ void _fs_write(uint8_t argc,char* argv[])
 	}
 	else
 	{
-		printf("Failed to open: %s\n", szPath);
+		printf("Failed to open: %s\n",argv[1]);
 	}
 }
 
 void _fs_list(uint8_t argc,char* argv[])
 {
-	DIR* pDir = opendir("/sf/");
+	if(argc < 2)
+	{
+		printf("needs directory name\n");
+		return;
+	}
+
+	DIR* pDir = opendir(argv[1]);
 	struct dirent* pEntry;
 	struct stat st;
 	if(pDir != NULL)
 	{
-		char szPath[MAX_PATH_LEN];
 		while((pEntry = readdir(pDir)) != NULL)
 		{
-			snprintf(szPath,MAX_PATH_LEN,"/sf/%s",(char*)(pEntry->d_name));
+#if 1
+			printf("%s\n",pEntry->d_name);
+#else
+			snprintf(szPath,MAX_PATH_LEN,"/fs/%s",(char*)(pEntry->d_name));
 			stat(szPath, &st);
 			printf("%s (%ld bytes)\n",pEntry->d_name,st.st_size);
+#endif
 		}
 		closedir(pDir);
+	}
+	else
+	{
+		printf("directory open failed: %s\n", argv[1]);
 	}
 }
 
@@ -97,9 +123,14 @@ void _fs_delete(uint8_t argc,char* argv[])
 		printf("needs file name\n");
 		return;
 	}
-	char szPath[MAX_PATH_LEN];
-	sprintf(szPath,"/sf/%s",argv[1]);
-	remove(szPath);
+	if(0 == unlink(argv[1]))
+	{
+		printf("delete %s done\n",argv[1]);
+	}
+	else
+	{
+		printf("delete %s failed\n",argv[1]);
+	}
 }
 
 void _fs_read(uint8_t argc,char* argv[])
@@ -109,10 +140,8 @@ void _fs_read(uint8_t argc,char* argv[])
 		printf("needs file name\n");
 		return;
 	}
-	char szPath[MAX_PATH_LEN];
-	sprintf(szPath,"/sf/%s",argv[1]);
 
-	FILE* pFile = fopen(szPath,"r");
+	FILE* pFile = fopen(argv[1],"r");
 
 	if(NULL != pFile)
 	{
@@ -135,7 +164,7 @@ void _fs_read(uint8_t argc,char* argv[])
 	}
 	else
 	{
-		printf("Error while open: %s\n", szPath);
+		printf("Error while open: %s\n",argv[1]);
 	}
 }
 
@@ -148,11 +177,8 @@ void _fs_dump(uint8_t argc,char* argv[])
 		printf("needs file name\n");
 		return;
 	}
-	char szPath[MAX_PATH_LEN];
-	sprintf(szPath, "/sf/%s", argv[1]);
-	printf("Open: %s\n", szPath);
 
-	FILE* pFile = fopen(szPath,"r");
+	FILE* pFile = fopen(argv[1],"r");
 
 	if(NULL != pFile)
 	{
@@ -179,23 +205,34 @@ void _fs_dump(uint8_t argc,char* argv[])
 	}
 	else
 	{
-		printf("Error while open: %s\n",szPath);
+		printf("Error while open: %s\n",argv[1]);
 	}
 }
 
 esp_err_t DOS_Init()
 {
+#if (FS_LFS)
+	esp_vfs_littlefs_conf_t conf =
+	{
+		.base_path = "/fs",
+		.partition_label = FS_LABEL,
+		.format_if_mount_failed = true,
+		.dont_mount = false,
+	};
+	esp_err_t ret = esp_vfs_littlefs_register(&conf);
+#else
 	esp_vfs_spiffs_conf_t conf =
 	{
-		.base_path = "/sf",
+		.base_path = "/fs",
 		.partition_label = FS_LABEL,
 		.max_files = 5,
 		.format_if_mount_failed = true
 	};
-
 	esp_err_t ret = esp_vfs_spiffs_register(&conf);
+#endif
 	if(ESP_OK == ret)
 	{
+		printf("Mount FS to %s\n", conf.base_path);
 		CLI_Register("fs_info",_fs_info);
 		CLI_Register("fs_format",_fs_format);
 		CLI_Register("fs_read",_fs_read);
